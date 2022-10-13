923f50e94f1b98f36ad9dc257ebd9b1bb33d010b
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-10-13 15:59:20 -0700

    Update for Testimonials page

--- views/pages/testimonials.php
+++ views/pages/testimonials.php
@@ -13,3 +13,29 @@
         </blockquote>
     </div>
 </header>
+
+<main id="main" class="main" role="main">
+
+    <section class="testimonials-list | uk-section">
+        <div class="uk-container">
+
+            <blockquote>
+                <h3>I was the owner of an owner-user industrial building that I had operated my business out of for nearly 50 years but it was time to finally retire. For this reason, I engaged Mark to list the property for sale.</h3>
+                <p>Mark took on the responsibility of listing our property and it became apparent that his efforts were equal to none. He facilitated all inspections, showings, contract negotiation, and deadlines. Additionally, he kept my whole family updated weekly with PDF’s outlining the market activity on the property, how many showings took place, and feedback from potential buyers. Mark’s communication efforts kept us fully informed with information about the listing process.</p>
+                <p>Once the property went under contract, Mark was able facilitate a quick and stress-free closing. Due to the time that I owned and operated the property, there was a significant amount of equipment, tools, scrap, and trash located in the building and in the lot. Because I’m not as spry as I used to be, it was difficult for me to remove all of these items before closing. Mark, Drew, Scott, and Dan arranged to help me get the rest of the items out of the space and worked for hours to remove the scrap steel, wood, and other trash so we could close on time – I’m not sure any other real estate agent would have done that!</p>
+                <p>Overall, Mark is a person of high ethical, moral, and professional standards, which is reflected in his work. As such, his outstanding client focus and personal service, impressive market knowledge, outstanding work ethic, and high level of integrity served me well. Mark has earned my highest recommendation as a Commercial Broker, and as a result, I would highly recommend Goodman Commercial Real Estate, Inc for your consideration as your real estate professional.</p>
+                <footer>Jim North, Owner <span class="uk-text-meta">3203 N Oakland Industrial Building</span></footer>
+            </blockquote>
+
+            <blockquote>
+                <h3>I was the owner of a high vacancy retail property that I had owned for quite some time and was ready to dispose of. For this reason, I engaged Mark to list the property for sale.</h3>
+                <p>Mark took on the responsibility of listing our property and it became apparent that his efforts were far to none. He facilitated all inspections, showings, contract negotiation, and deadlines. Additionally, he kept our whole team updated weekly with PD F’s outlining the market activity on the property, how many showings took place, and feedback from potential buyers. Marks communication efforts kept us fully informed with information about the listing process.</p>
+                <p>Once the property went under contract, Mark was able facilitate a quick and stress-free closing.</p>
+                <p>Overall, Mark is a person of high ethical, moral, and professional standards, which is reflected in his work. As such, his outstanding client focus and personal service, impressive market knowledge, outstanding work ethic, and high level of integrity served me well. Mark has earned my highest recommendation as a Commercial Broker, and as a result, I would highly recommend Goodman Commercial Real Estate, Inc for your consideration as your real estate professional.</p>
+                <footer>Richard Huffman, Managing Partner <span class="uk-text-meta">Huffman Family Partnership</span></footer>
+            </blockquote>
+
+        </div>
+    </section>
+
+</main>
