5f3e6c0370bdf91eefaa4f3bfe711ca7527d5e1f
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-10-13 13:53:54 -0700

    Update background headers for other pages

---
+++ views/pages/faq.php
@@ -0,0 +1,11 @@
+<header data-fragment="hero" class="faq-header | uk-position-relative">
+    <div class="uk-cover-container">
+        <img src="<?php echo _uri.'/resources/images/bg_header-faq.jpg'; ?>" alt="" uk-cover>
+        <canvas width="1920" height="980"></canvas>
+    </div>
+    <div class="uk-overlay-primary uk-position-cover"></div>
+    <div class="uk-overlay uk-position-bottom uk-light">
+        <h1>Frequently Asked Questions</h1>
+        <h2>Questions? <span class="uk-text-uppercase">Answers.</span></h2>
+    </div>
+</header>
