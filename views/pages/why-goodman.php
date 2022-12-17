411f75779d1b0960ff25def0672fbea5ea0420e6
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-12-18 05:52:29 +0800

    Update for content

---
+++ views/pages/why-goodman.php
@@ -0,0 +1,13 @@
+<main id="main" class="main" role="main">
+
+    <section class="why-goodman | uk-section uk-section-muted">
+        <div class="uk-container uk-container-small">
+
+            <article class="uk-article">
+                <?php the_field( 'why_goodman' ); ?>
+            </article>
+
+        </div>
+    </section>
+
+</main>
