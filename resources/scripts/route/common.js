32683c3576defcc7a6cd44fba1f878e203721bcb
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2023-02-14 12:18:22 +0800

    accessibility update

--- resources/scripts/route/common.js
+++ resources/scripts/route/common.js
@@ -17,6 +17,16 @@
                 }
             });
 
+            // Elfsight Search Input Accessibility Fix
+            jQuery('.eapps-google-maps-bar-search-inner').prepend('<label for="google-search">Search Property</label>').find('input').attr('id','google-search').attr('placeholder','Search Property');
+            jQuery('body .eapps-google-maps-bar-button-filter').find('div:first').text( 'Sort By Property Category' );
+
+            // Alt Tag for Image
+            jQuery('.eapps-google-maps-bar-list-item').each(function() {
+                var _title = jQuery(this).find('.eapps-google-maps-bar-list-item-info-title').text();
+                var _title = jQuery.trim(_title);
+                jQuery(this).find('.eapps-google-maps-bar-list-item-additional-picture img').attr('alt', _title);
+            });
         }    
     }, 100);
 
